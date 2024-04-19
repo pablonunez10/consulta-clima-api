741b6d0cdad40fdd62457bd040ee621dd5b32d75
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-19 10:53:54 -0400

    clima-test

---
+++ src/components/Form.tsx
@@ -0,0 +1,66 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import type { SearchType } from '../types'
+import { countries } from '../data/countries'
+import styles from './Form.module.css'
+import Alert from './Alert/Alert'
+type formProps = {
+    fetchWeather: (search: SearchType) => Promise<void>
+}
+export default function Form({fetchWeather} : formProps) {
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+    const [ alert, setAlert] = useState('')
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (Object.values(search).includes('')) {
+            setAlert('Todos los campos son obli')
+            return
+        }
+        fetchWeather(search)  
+    }
+  return (
+   <form 
+        className={styles.form}
+        onSubmit={handleSubmit}
+    >
+    {alert && <Alert>{alert}</Alert>}   
+    <div className={styles.field}>
+        <label htmlFor="city">Ciudad:</label>
+        <input 
+            id='city'
+            type="text" 
+            name='city'
+            placeholder='Ciudad'
+            value={search.city}
+            onChange={handleChange}
+        />
+    </div>
+    <div className={styles.field}>
+        <label htmlFor="country">Country:</label>
+        <select 
+            id='country'
+            value={search.country}
+            name='country'
+            onChange={handleChange}
+        >
+            <option value="">Select one Country</option>
+            {countries.map(country => (
+                <option
+                    key={country.code}     
+                    value={country.code}
+                >{country.name}</option>
+            ))}
+        </select>
+    </div>
+    <input type="submit" value='Consultar Clima' className={styles.submit} />
+   </form>
+  )
+}
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,114 @@
+import axios from 'axios'
+ import {z} from 'zod'
+// import {object, string, number, parse, Output   } from 'valibot'
+import { SearchType } from '../types'
+import { useState, useMemo } from 'react'
+        //TYPE GUARDS
+// function isWeatherResponse(weather : unknown) : weather is Weather {
+//     return (
+//         Boolean(weather) && 
+//         typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' && 
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number' 
+//     )
+// }
+
+//Zod
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp:z.number(),
+        temp_max:z.number(),
+        temp_min:z.number()
+    })
+})
+export type Weather = z.infer<typeof Weather>
+
+//Valibot
+// const WeatherSchema = object({
+//     name: string(),
+//     main: object({
+//         temp: number(),
+//         temp_max: number(),
+//         temp_min: number()
+//     }),
+// })
+// type Weather = Output<typeof WeatherSchema>
+
+const initialState = {
+    name: '',
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0,
+    }
+}
+export default function useWeather () {
+    const [weather, setWeather] = useState<Weather>(initialState)
+    const [loading, setLoading] = useState(false)
+    const [notFound, setNotFound] = useState(false)
+    const fetchWeather = async(search : SearchType) => {
+        const appId = import.meta.env.VITE_API_KEY
+        setLoading(true)
+        setWeather(initialState)
+        try {
+          const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}` 
+          const {data} = await axios(geoUrl) //el metodo que trae por default es el get
+          //Comprobar
+          if (!data[0]) {
+            setNotFound(true)
+            return
+          }
+          const lat = data[0].lat
+          const lon = data[0].lon
+          const weatherUrl =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+        
+        //CASTEAR EL TYPE
+        //   const {data: weatherUrlResult} = await axios<Weather>(weatherUrl)
+        //   console.log(weatherUrlResult.main.temp)
+        //   console.log(weatherUrlResult.name)
+          // console.log(weatherUrlResult.main.temp)
+        // console.log(weatherUrlResult.name)
+        
+        //TYPE GUARDS
+        // const {data: weatherResult} = await axios(weatherUrl)
+        // const result = isWeatherResponse(weatherResult)
+        // if(result) {
+        //     console.log(weatherResult.name)
+        // } else {
+        //     console.log('no hay rp')
+        // }
+
+        //ZOD
+        const {data: weatherResult} = await axios(weatherUrl)
+        const result = Weather.safeParse(weatherResult)
+        if (result.success) {
+            setWeather(result.data)
+        } else {
+            console.log('ta mal')
+        }
+
+        //Valibot
+        // const {data: weatherResult} = await axios(weatherUrl)
+        // const result = parse(WeatherSchema, weatherResult )
+        // if (result) {
+        //     console.log(result.name)
+        //     console.log(result.main.temp)
+        // }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+    const hasWeatherData = useMemo(() => weather.name, [weather])
+    return {
+        weather,
+        loading,
+        notFound,
+        fetchWeather,
+        hasWeatherData
+    }
+}
